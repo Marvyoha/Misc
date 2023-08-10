a216889db2e994fb751ca08bae9c2dcf771eb127
Olewuezi Marvellous <Marvyoha1@gmail.com>
2023-08-10 10:08:05 +0100

    Added Avatar glow and percent indicator to the splashscreen

--- cosmic/lib/splash.dart
+++ cosmic/lib/splash.dart
@@ -1,5 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:cosmic/Login.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
@@ -10,8 +12,7 @@ class Splash extends StatelessWidget {
 
     // Adding a delay before navigating to the next page
 
-    Future.delayed(const Duration(milliseconds: 1700), () {
-      // Replace 'NextPage' with the actual page you want to navigate to
+    Future.delayed(const Duration(milliseconds: 2500), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const Login()),
@@ -20,25 +21,46 @@ class Splash extends StatelessWidget {
 
     return Scaffold(
       body: Container(
-        height: size.height,
-        width: size.width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/assets/Cosmic Background.png'),
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: ShapeDecoration(
-            shape: CircleBorder(
-              side: BorderSide(width: 7, color: Colors.white.withOpacity(0.1)),
+          height: size.height,
+          width: size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/assets/Cosmic Background.png'),
+              fit: BoxFit.fill,
             ),
           ),
-          child: Image.asset('lib/assets/Cosmic logo.png'),
-        ),
-      ),
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                AvatarGlow(
+                  endRadius: 300,
+                  duration: const Duration(milliseconds: 2100),
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                    decoration: ShapeDecoration(
+                      shape: CircleBorder(
+                        side: BorderSide(
+                            width: 1, color: Colors.white.withOpacity(0.1)),
+                      ),
+                    ),
+                    child: Image.asset(
+                      'lib/assets/Cosmic logo.png',
+                    ),
+                  ),
+                ),
+                CircularPercentIndicator(
+                  radius: 204,
+                  percent: 0.78,
+                  lineWidth: 8,
+                  progressColor: Colors.white.withOpacity(0.5),
+                  backgroundColor: Colors.transparent,
+                  circularStrokeCap: CircularStrokeCap.round,
+                )
+              ],
+            ),
+          )),
     );
   }
 }
